9435cc7072071335b43306d7b837186e25ff8c4d
kelving <gtainguyenk19@gmail.com>
2025-10-14 00:03:42 +0700

    Done Day7

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// namespace MyApp
+// {
+
+//     public class InvalidPriceException : Exception
+//     {
+//         public InvalidPriceException(string message) : base(message)
+//         {
+
+//         }
+//     }
+
+//     class Product
+//     {
+//         public string? Name { get; set; }
+//         private double _price;
+//         public double Price
+//         {
+//             get => _price;
+//             set
+//             {
+//                 if (value < 0)
+//                 {
+//                     throw new InvalidPriceException("Giá sản phẩm không thể âm!");
+//                 }
+//                 _price = value;
+//             }
+//         }
+//         static void Main()
+//         {
+//             // exception handling
+//             try
+//             {
+//                 int a = 10;
+//                 int b = 0;
+//                 int result = a / b;
+//             }
+//             catch (DivideByZeroException e)
+//             {
+//                 Console.WriteLine($"eRROR: {e.Message}");
+//             }
+//             finally
+//             {
+//                 Console.WriteLine("Khối finally luôn được thực thi.");
+//             }
+
+//             Product product = new();
+//             product.Price = -3;
+//         }
+//     }
+// }
